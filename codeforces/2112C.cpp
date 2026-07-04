ae26c0ac1b67c5d55d96aae269dfe06c062d8a3b
Soham Aggarwal <aggarwalsoham3@gmail.com>
2026-07-04 17:21:06 +0530

    .

---
+++ codeforces/2112C.cpp
@@ -0,0 +1,46 @@
+//        /\_/|
+//       ( •_• )   SOHAM AGGARWAL
+//      / >   >    gf said "commit"
+//                 so I pushed to GitHub
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<long long>;
+
+const int MOD=1e9+7;
+const int INF = 1e9;
+const ll LINF = 4e18;
+
+#define all(c) (c).begin(), (c).end()
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+
+void solve() {
+    int n; cin>>n;
+    vi a(n); rep(i,0,n) cin>>a[i];
+    ll total=0;
+    rep(k,2,n) {
+        int need=max(a[k],a[n-1]-a[k]);
+        int i=0,j=k-1;
+        while(i<j) {
+            if(a[i]+a[j]>need) {
+                total+=j-i;
+                j--;
+            }
+            else {
+                i++;
+            }
+        }
+    }
+    cout<<total<<endl;
+}   
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t; cin>>t;
+    while(t--) solve();
+}
