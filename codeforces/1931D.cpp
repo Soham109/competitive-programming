a883ce4e033bff8e1697716b37539e0a3957fe69
Soham Aggarwal <aggarwalsoham3@gmail.com>
2026-07-23 18:25:29 +0530

    .

---
+++ codeforces/1931D.cpp
@@ -0,0 +1,42 @@
+//        /\_/|
+//       ( •_• )   SOHAM AGGARWAL
+//      / >   >    gf said "commit"
+//                 so I pushed to GitHub
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<long long>;
+
+const int MOD=1e9+7;
+const int INF = 1e9;
+const ll LINF = 4e18;
+
+#define all(c) (c).begin(), (c).end()
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+
+void solve() {
+    int n,x,y; cin>>n>>x>>y;
+    vi a(n); rep(i,0,n) cin>>a[i];
+
+    //ai%x + aj%x -> 0 or x
+    //ai%y = aj%y
+
+    map<ll,map<ll,ll>> mp;
+    ll ans=0;
+    rep(i,0,n){
+        ans+=mp[(x-a[i]%x)%x][a[i]%y];
+        mp[a[i]%x][a[i]%y]++;
+    }
+    cout<<ans<<endl;
+}   
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t; cin>>t;
+    while(t--) solve();
+}
