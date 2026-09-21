92595fe5e19dcd82f82a593ab7dd2c1102bfb3ef
Soham Aggarwal <aggarwalsoham3@gmail.com>
2026-09-21 13:57:58 -0500

    1407B: restore my own solution and match the write-up to it

--- codeforces/1407B.cpp
+++ codeforces/1407B.cpp
@@ -15,28 +15,45 @@ const int MOD=1e9+7;
 const int INF = 1e9;
 const ll LINF = 4e18;
 
-#define all(c) (c).begin(), (c).end()
+#define all(c) (c).rbegin(), (c).rend()
 #define rep(i, a, b) for (int i = (a); i < (b); i++)
+
+//observaitons:
+// - decreasing order better for number with common gcds etc.
+// - first element outputed will be max element
+// - algo:
+// sort in decreasing order. after first element, output the elemtn with max gcd
+// and if 2 elements have same gcd with it,
+
+
 
 void solve() {
     int n; cin>>n;
     vi a(n); rep(i,0,n) cin>>a[i];
+    sort(all(a));
 
     vi used(n,0);
-    int g = 0;
+    vi ans(n);
 
-    rep(step,0,n) {
-        int best = -1;
+    ans[0] = a[0]; used[0] = 1;
+    
+    int g = a[0];
+    rep(i,1,n) {
+        int mx=0;
+        int idx=0;
         rep(j,0,n) {
-            if(used[j]) continue;
-            if(best==-1 || gcd(g,a[j])>gcd(g,a[best])) best=j;
+            if(used[j] == 0 && gcd(g,a[j])>mx) {
+                idx=j;
+                mx = gcd(g,a[j]);
+            }
         }
-        used[best] = 1;
-        g = gcd(g,a[best]);
-        cout<<a[best]<<" ";
+        g = gcd(g,a[idx]);
+        used[idx] = 1;
+        ans[i] = a[idx];
     }
-    cout<<"\n";
-}
+    rep(i,0,n) cout<<ans[i]<<" ";
+    cout<<endl;
+}   
 
 int main() {
     ios::sync_with_stdio(false);
